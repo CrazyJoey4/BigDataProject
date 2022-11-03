b0de09205d50bdc4963fe930f83ee42d525ea148
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-03 22:57:30 +0800

    Try and trial

--- BigDataProgrammingProject.R
+++ BigDataProgrammingProject.R
@@ -2,18 +2,32 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(tidyr)
+library(data.table)
+library(tidyverse)
+library(readr)
+library(magrittr)
+
+#change directory
+setwd("~/BigData/BigDataProject")
 
 #import csv files
 
-#Nutrition
-tesco_msoa<-read.csv("./Area-level grocery purchases/year_msoa_grocery.csv")
-tesco_ward<-read.csv("./Area-level grocery purchases/year_osward_grocery.csv")
-tesco_oslaua<-read.csv("./Area-level grocery purchases/year_borough_grocery.csv")
+#Grocery
+year_osward_grocery<-read.csv("./BigDataProject/Area-level grocery purchases/year_osward_grocery.csv")
+
 #Health Data
-child_obesity_ward<-read.csv("./Validation data (obesity, diabetes)/child_obesity_london_ward_2013-2014.csv")
-child_obesity_oslaua<-read.csv("./Validation data (obesity, diabetes)/child_obesity_london_oslaua_2015-2016.csv")
-adult_obesity_oslaua<-read.csv("./Validation data (obesity, diabetes)/london_obesity_oslaua_2012.csv")
-adult_obesity_hospital_oslaua<-read.csv("./Validation data (obesity, diabetes)/obesity_hospitalization_oslaua_2016.csv")
-diabetes_ward<-read.csv("./Validation data (obesity, diabetes)/diabetes_estimates_osward_2016.csv")
+diabetes_estimates_osward_2016<-read.csv("./Validation data (obesity, diabetes)/diabetes_estimates_osward_2016.csv")
+
+
+# Using read.csv()
+list_csv_files <- list.files(path = "./Validation data (obesity, diabetes)/")
+df2 = do.call(rbind, lapply(list_csv_files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+df2
+
+df <-
+  list.files(path = "Validation data (obesity, diabetes)/", pattern = "*.csv") %>% 
+  map_df(~read_csv(.))
+df
+
 
 
