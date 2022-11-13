aec81fe9574704936553134434401a02e39aacb1
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-13 20:46:17 +0800

    Update BigDataProgrammingProject.R

--- BigDataProgrammingProject.R
+++ BigDataProgrammingProject.R
@@ -22,6 +22,7 @@ library(manipulate)
 
 gc()
 
+stopCluster(cl)
 
 #change directory
 setwd("~/BigData")
@@ -35,6 +36,7 @@ diabetes_estimates_osward_2016<-read.csv("./Validation data (obesity, diabetes)/
 setwd("~/BigData/Area-level grocery purchases")
 ListFile <- 
   list.files(path=".", pattern="*.csv", all.files=TRUE, full.names=FALSE)
+
 GPData = do.call(rbind, lapply(ListFile, function(x) read.csv(x, stringsAsFactors = FALSE)))
 
 
@@ -48,21 +50,34 @@ Seqfunction <- function(x)
   do.call(rbind, lapply(ListFile, function(x) read.csv(x, stringsAsFactors = FALSE)))
 }
 
-Seq_TT <- system.time(Seqfunction())
+system.time(Seq_TT <- Seqfunction)
 
 
 #Parallel Process
 Parfunction <- function(y)
 {
-  cl_area <- detectCores(ListFile)
-  cl <- makeCluster(cl_area)        #takes in as an argument the number of cores
-  clusterEvalQ(cl, 2 + 2)           #takes a cluster and any expression, and executes the expression on each process
+  YearFile <- 
+    list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE)
   
-  clusterEvalQ(cl, {do.call(rbind, lapply(list.files(path=".", pattern="*.csv", all.files=TRUE, full.names=FALSE), function(x) read.csv(x, stringsAsFactors = FALSE)))})
+  cl_area <- detectCores(YearFile)
+  cl <- makeCluster(cl_area)        #takes in as an argument the number of cores
+  clusterEvalQ(cl, 
+               {library(ggplot2)
+                library(stringr)
+                 })       #takes a cluster and any expression, and executes the expression on each process
+  
+}
+
+system.time(Par_TT <- parLapply(1:100, Parfunction))
+  
+Par_TT
+  
+  
+  clusterEvalQ(cl, {list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE), function(x) read.csv(x, stringsAsFactors = FALSE)})
+  
+  parLapply(cl, 1:100, list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE), function(x) read.csv(x, stringsAsFactors = FALSE))
 }
 
 Par_TT <- system.time(parLapply(cl, 1:100, Parfunction()))
-
-
 
 
