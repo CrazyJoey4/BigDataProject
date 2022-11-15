ecf2bc80277c4c1e72e930de1d9fceb29891a653
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-16 04:16:26 +0800

    Update BigDataProgrammingProject.R

--- BigDataProgrammingProject.R
+++ BigDataProgrammingProject.R
@@ -20,9 +20,6 @@ library(useful)
 library(ff)
 library(manipulate)
 
-gc()
-
-stopCluster(cl)
 
 #change directory
 setwd("~/BigData")
@@ -33,7 +30,7 @@ year_osward_grocery<-read.csv("./Area-level grocery purchases/year_osward_grocer
 diabetes_estimates_osward_2016<-read.csv("./Validation data (obesity, diabetes)/diabetes_estimates_osward_2016.csv")
 
 #Import Grocery Purchases
-setwd("~/BigData/Area-level grocery purchases")
+setwd("~/BigData/Area-level grocery purchases/Yearly")
 ListFile <- 
   list.files(path=".", pattern="*.csv", all.files=TRUE, full.names=FALSE)
 
@@ -41,9 +38,9 @@ GPData = do.call(rbind, lapply(ListFile, function(x) read.csv(x, stringsAsFactor
 
 
 #Sequential Process
-mbm <- microbenchmark("Sequential Process" = {do.call(rbind, lapply(ListFile, function(x) read.csv(x, stringsAsFactors = FALSE)))})
-mbm
-autoplot(mbm)
+Seq <- microbenchmark("Sequential Process" = {do.call(rbind, lapply(ListFile, function(x) read.csv(x, stringsAsFactors = FALSE)))})
+Seq
+autoplot(Seq)
 
 Seqfunction <- function(x)
 {
@@ -54,30 +51,28 @@ system.time(Seq_TT <- Seqfunction)
 
 
 #Parallel Process
+setwd("~/BigData/Area-level grocery purchases/Yearly")
+YearFile <- 
+  list.files(path=".", pattern="*.csv", all.files=TRUE, full.names=FALSE) %>%
+  map_df(~read_csv(.))
+
+YearFile
+
+cl_area <- detectCores(YearFile)
+cl <- makeCluster(cl_area)        #takes in as an argument the number of cores
+clusterEvalQ(cl, {
+  library(ggplot2)
+  library(stringr)
+  })                              #takes a cluster and any expression, and executes the expression on each process
+  
 Parfunction <- function(y)
 {
-  YearFile <- 
-    list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE)
-  
-  cl_area <- detectCores(YearFile)
-  cl <- makeCluster(cl_area)        #takes in as an argument the number of cores
-  clusterEvalQ(cl, 
-               {library(ggplot2)
-                library(stringr)
-                 })       #takes a cluster and any expression, and executes the expression on each process
-  
-}
-
-system.time(Par_TT <- parLapply(1:100, Parfunction))
-  
-Par_TT
-  
-  
-  clusterEvalQ(cl, {list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE), function(x) read.csv(x, stringsAsFactors = FALSE)})
-  
-  parLapply(cl, 1:100, list.files(path="~/BigData/Area-level grocery purchases/Yearly", pattern="*.csv", all.files=TRUE, full.names=FALSE), function(x) read.csv(x, stringsAsFactors = FALSE))
+  parLapply(cl, 1:100, YearFile)
+  stopCluster(cl)
 }
 
 Par_TT <- system.time(parLapply(cl, 1:100, Parfunction()))
+
+Par <- microbenchmark("Parallel " = {parLapply(cl, 1:100, AL_Grocery2)})
 
 
